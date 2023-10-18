5ec8a7e66601fdc9265e50582bbbf200e7aebccb
Paul Boekschoten <paul.boekschoten@hashicorp.com>
2023-10-18 11:34:33 +0200

    diagram added

--- diagram/diagram.py
+++ diagram/diagram.py
@@ -2,25 +2,63 @@
 
 from diagrams import Cluster, Diagram
 from diagrams.aws.general import Client
-from diagrams.aws.compute import EC2
+from diagrams.aws.network import Route53
+from diagrams.aws.network import InternetGateway
+from diagrams.aws.network import NATGateway
+from diagrams.aws.network import ElbApplicationLoadBalancer
+from diagrams.aws.compute import EC2AutoScaling
 from diagrams.aws.database import RDSPostgresqlInstance
+from diagrams.aws.database import ElasticacheForRedis
 from diagrams.aws.storage import SimpleStorageServiceS3Bucket
 
 
-with Diagram("TFE Airgapped", show=False, direction="TB"):
+with Diagram("TFE Active-active", show=False, direction="TB"):
     
-    client = Client("Client")
+    user = Client("User")
 
     with Cluster("AWS"):
+
+        route53 = Route53("DNS record")
+
         with Cluster("VPC"):
-            with Cluster("Public Subnet"):
-                tfe_instance = EC2("Terraform Enterprise")
+            igw = InternetGateway("Internet Gateway")
+
+            #AZ:a
+            with Cluster("Availability Zone: eu-west3a"):
+              with Cluster("Public Subnet 2"):
+                alb_a = ElbApplicationLoadBalancer("ALB")
             
-            with Cluster("Private Subnet"):
-                postgres = RDSPostgresqlInstance("PostgresSQL")
+              with Cluster("Private Subnet 2"):
+                asg_a = EC2AutoScaling("Autoscaling Group \n TFE instance")
+                postgres_a = RDSPostgresqlInstance("PostgresSQL")
+                redis_a = ElasticacheForRedis("Redis")
 
-        s3bucket = SimpleStorageServiceS3Bucket("S3 bucket")
+            #AZ:a
+            with Cluster("Availability Zone: eu-west3c"):
+              with Cluster("Public Subnet"):
+                nat = NATGateway("NAT gateway")
+                alb_c = ElbApplicationLoadBalancer("ALB")
+            
+              with Cluster("Private Subnet"):
+                asg_c = EC2AutoScaling("Autoscaling Group \n TFE instance")
+                #postgres_c = RDSPostgresqlInstance("PostgresSQL")
+                #redis_c = ElasticacheForRedis("Redis")
 
-    client >> tfe_instance
-    tfe_instance >> postgres
-    tfe_instance >> s3bucket
+        s3bucket = SimpleStorageServiceS3Bucket("TFE bucket")
+        s3bucketfiles = SimpleStorageServiceS3Bucket("TFE install \n files bucket")
+
+    user >> route53
+    user >> alb_a
+    user >> alb_c
+    alb_a >> asg_a
+    alb_a >> asg_c
+    alb_c >> asg_a
+    alb_c >> asg_c
+    asg_a >> postgres_a
+    asg_c >> postgres_a
+    asg_a >> redis_a
+    asg_c >> redis_a
+    asg_a >> s3bucket
+    asg_c >> s3bucket
+    asg_a >> s3bucketfiles
+    asg_c >> s3bucketfiles
